086e55e5be11ab5fc84cd1ab9d6c3b5844bef6a6
Abhiroop Santra <abhiroop43@users.noreply.github.com>
2023-12-27 00:21:07 +0400

    chore: initial commit

---
+++ src/main/java/dev/abhiroopsantra/schoolmgmtapi/SchoolmgmtapiApplication.java
@@ -0,0 +1,13 @@
+package dev.abhiroopsantra.schoolmgmtapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolmgmtapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolmgmtapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/abhiroopsantra/schoolmgmtapi/SchoolmgmtapiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.abhiroopsantra.schoolmgmtapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolmgmtapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
