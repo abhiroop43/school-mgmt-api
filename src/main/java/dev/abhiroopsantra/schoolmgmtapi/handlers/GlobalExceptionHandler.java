9be5d7425a86a5b3ddb9a3816152c21387eb142b
Abhiroop Santra <abhiroop43@users.noreply.github.com>
2024-01-06 11:29:17 +0400

    refactor: Improve exception handling and logging

--- src/main/java/dev/abhiroopsantra/schoolmgmtapi/handlers/GlobalExceptionHandler.java
+++ src/main/java/dev/abhiroopsantra/schoolmgmtapi/handlers/GlobalExceptionHandler.java
@@ -9,24 +9,33 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 @RestControllerAdvice public class GlobalExceptionHandler {
+
+    Logger logger = Logger.getLogger(GlobalExceptionHandler.class.getName());
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ApiResponse> handleNotFoundException(NotFoundException ex) {
+        logger.log(Level.WARNING, "No Data Found", ex);
         return new ResponseEntity<>(new ApiResponse(null, "1", ex.getMessage()), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(Exception.class) public ResponseEntity<ApiResponse> handleException(Exception ex) {
+        logger.log(Level.SEVERE, "Internal Server Error", ex);
         return new ResponseEntity<>(new ApiResponse(null, "2", ex.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(UnauthorizedException.class)
     public ResponseEntity<ApiResponse> handleUnauthorizedException(UnauthorizedException ex) {
+        logger.log(Level.WARNING, "Unauthorized Access to API", ex);
         return new ResponseEntity<>(new ApiResponse(null, "3", ex.getMessage()), HttpStatus.FORBIDDEN);
     }
 
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<ApiResponse> handleBadRequestException(BadRequestException ex) {
+        logger.log(Level.SEVERE, "Request was invalid", ex);
         return new ResponseEntity<>(new ApiResponse(null, "4", ex.getMessage()), HttpStatus.BAD_REQUEST);
     }
 }
